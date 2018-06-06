81c476721ccf2e9d6cc564f27869fbbd14a6c41c
Rodrigo <Rodrigo Ferreira>
2018-06-06 18:28:20 -0300

    iteration method almost done

--- SIRModel.py
+++ SIRModel.py
@@ -20,34 +20,76 @@ class SIRModel:
         self.graph = graph
         self.beta = beta
         self.gamma = gamma
+        self.status = []
 
         self.susceptibles = range(graph.vcount())
         self.infecteds = []
         self.recovereds = []
 
         self.nodes_neighbors = []
-        for i in xrange(graph.vcount()):
-            self.nodes_neighbors.append({'infected_neighbors': 0, 'susceptible_neighbors': self.graph.neighbors(i)})
+        for i in range(graph.vcount()):
+            self.nodes_neighbors.append(
+                {'infected_neighbors': 0,
+                 'susceptible_neighbors': [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(i)))]})
+
+        # for i, e in enumerate(self.nodes_neighbors):
+        #     print i,": ",e
 
     def reset(self):
         pass
 
     def iterate(self):
-        pass
+        """
+            Infection and recovery Process
+        :return: status
+        """
+        #infection
+        print "Numero de infectados: ", len(self.infecteds)
+        for i in range(len(self.infecteds)):
+            print "Infectados: ", self.infecteds
+            print "Eu deveria percorrer:"
+            neighbors = self.nodes_neighbors[self.infecteds[i]]['susceptible_neighbors'][:]
+            print neighbors
+
+            #for neighbor in self.nodes_neighbors[self.infecteds[i]]['susceptible_neighbors']:
+            for neighbor in neighbors:
+                event = np.random.random_sample()
+                print "No: ", self.infecteds[i]
+                print "Vizinho: ", neighbor
+                # print "Event: ", event
+                # print "Conta: ", self.beta * self.nodes_neighbors[neighbor]['infected_neighbors']
+                # print "Vizinhos: ", self.nodes_neighbors[neighbor]['susceptible_neighbors']
+
+                if event < (self.beta * self.nodes_neighbors[neighbor]['infected_neighbors']):
+                    self.susceptibles.remove(neighbor)
+                    self.infecteds.append(neighbor) # erro aqui
+                    print neighbor, " foi infectado"
+
+                    for n in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(neighbor)))]:
+                        self.nodes_neighbors[n]['susceptible_neighbors'].remove(neighbor)
+                        self.nodes_neighbors[n]['infected_neighbors'] += 1
+                print "--------------------------"
+
+        self.iteration += 1
+        return (len(self.susceptibles), len(self.infecteds), len(self.recovereds))
 
     def infect(self, infecteds):
         for node in infecteds:
             self.infecteds.append(node)
 
+            #print "Infectado: ", node
+
             if node in self.recovereds:
                 self.recovereds.remove(node)
 
-                for neighbor in self.graph.neighbors(node):
+                for neighbor in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(node)))]:
                     self.nodes_neighbors[neighbor]['infected_neighbors'] += 1
             else:
                 self.susceptibles.remove(node)
 
-                for neighbor in self.graph.neighbors(node):
+                #print "Vizinhos de ", node, ": ", [int(v['name']) for v in self.graph.vs[node].neighbors()]
+
+                for neighbor in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(node)))]:
                     self.nodes_neighbors[neighbor]['susceptible_neighbors'].remove(node)
                     self.nodes_neighbors[neighbor]['infected_neighbors'] += 1
 
@@ -58,12 +100,12 @@ class SIRModel:
             if node in self.infecteds:
                 self.infecteds.remove(node)
 
-                for neighbor in self.graph.neighbors(node):
+                for neighbor in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(node)))]:
                     self.nodes_neighbors[neighbor]['infected_neighbors'] -= 1
             else:
                 self.susceptibles.remove(node)
 
-                for neighbor in self.graph.neighbors(node):
+                for neighbor in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(node)))]:
                     self.nodes_neighbors[neighbor]['susceptible_neighbors'].remove(node)
 
 
--- test.py
+++ test.py
@@ -2,7 +2,23 @@ from SIRModel import SIRModel
 from igraph import *
 
 graph = Graph.Read_Ncol('moreno_highschool.txt', directed=False)
-sirmodel = SIRModel(graph, 0.1, 0.2)
+#print graph.vs[0].neighbors()[6]['name']
+#print graph.neighbors(0)
+
+
+#new_range  = [i * i          for i in range(5)   if i % 2 == 0]
+# neighbors = [int(v['name']) for v in graph.vs[0].neighbors()]
+# print neighbors
+
+# for v in graph.neighbors(graph.vs.find('19')):
+#     print graph.vs[v]['name']
+
+beta = 0.2857
+gamma = 0.1428
+
+sirmodel = SIRModel(graph, beta, gamma)
+
+#print graph
 
 # print sirmodel.beta
 # print sirmodel.gamma
@@ -10,7 +26,7 @@ sirmodel = SIRModel(graph, 0.1, 0.2)
 #     print n
 #
 # print "\n########################\n"
-# sirmodel.infect([0,1])
+sirmodel.infect([0, 1])
 #
 # for n in sirmodel.nodes_neighbors:
 #     print n
@@ -21,18 +37,19 @@ sirmodel = SIRModel(graph, 0.1, 0.2)
 # print "I: ", sirmodel.infecteds
 # print "R: ", sirmodel.recovereds
 
-for n in sirmodel.nodes_neighbors:
-    print n
+# for n in sirmodel.nodes_neighbors:
+#     print n
 
-sirmodel.recover([1,0,68,69])
-sirmodel.infect([0,1])
+#sirmodel.recover([1,0,68,69])
+#sirmodel.infect([0])
+print sirmodel.iterate()
 
 
-print "\n########################\n"
+# print "\n########################\n"
+#
+# for n in sirmodel.nodes_neighbors:
+#     print n
 
-for n in sirmodel.nodes_neighbors:
-    print n
-
-# print "S: ", sirmodel.susceptibles
-# print "I: ", sirmodel.infecteds
-# print "R: ", sirmodel.recovereds
+print "S: ", sirmodel.susceptibles
+print "I: ", sirmodel.infecteds
+print "R: ", sirmodel.recovereds
