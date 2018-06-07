501534b8797b3aef16205b3efa920ab1becc8a3a
Rodrigo <Rodrigo Ferreira>
2018-06-07 17:37:55 -0300

    tests running

--- SIRModel.py
+++ SIRModel.py
@@ -56,19 +56,35 @@ class SIRModel:
                 event = np.random.random_sample()
                 print "No: ", self.infecteds[i]
                 print "Vizinho: ", neighbor
-                # print "Event: ", event
-                # print "Conta: ", self.beta * self.nodes_neighbors[neighbor]['infected_neighbors']
-                # print "Vizinhos: ", self.nodes_neighbors[neighbor]['susceptible_neighbors']
+                print "Event: ", event
+                print "Conta: ", self.beta * self.nodes_neighbors[neighbor]['infected_neighbors']
+                print "Vizinhos: ", self.nodes_neighbors[neighbor]['susceptible_neighbors']
 
                 if event < (self.beta * self.nodes_neighbors[neighbor]['infected_neighbors']):
+                    self.infecteds.append(neighbor)  # erro aqui
                     self.susceptibles.remove(neighbor)
-                    self.infecteds.append(neighbor) # erro aqui
                     print neighbor, " foi infectado"
 
                     for n in [int(self.graph.vs[v]['name']) for v in self.graph.neighbors(self.graph.vs.find(str(neighbor)))]:
                         self.nodes_neighbors[n]['susceptible_neighbors'].remove(neighbor)
                         self.nodes_neighbors[n]['infected_neighbors'] += 1
                 print "--------------------------"
+
+        #recovery
+        # print "\n ------------------------\n Recovery:"
+        # print "Devo percorrer: "
+        # print self.infecteds
+        for i in reversed(range(len(self.infecteds))):
+            event = np.random.random_sample()
+            # print "No: ", self.infecteds[i]
+            if event < self.gamma:
+                # print "No ", self.infecteds[i], " foi recuperado"
+                # print "Event: ", event
+                # print "Gamma: ", self.gamma
+                # print "---------------------"
+                self.recovereds.append(self.infecteds[i])
+                self.infecteds.remove(self.infecteds[i])
+
 
         self.iteration += 1
         return (len(self.susceptibles), len(self.infecteds), len(self.recovereds))
--- test.py
+++ test.py
@@ -1,9 +1,34 @@
 from SIRModel import SIRModel
 from igraph import *
+from matplotlib import pyplot as plt
+import networkx as nx
+import numpy as np
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics.SIRModel as sir
 
-graph = Graph.Read_Ncol('moreno_highschool.txt', directed=False)
+graph = Graph.Read_Ncol('email_enron.txt', directed=False)
+# graph = nx.read_edgelist('email_enron.txt', nodetype = int)
 #print graph.vs[0].neighbors()[6]['name']
 #print graph.neighbors(0)
+
+print graph.summary()
+
+beta = 0.2857
+gamma = 0.1428
+
+
+# model = sir.SIRModel(graph)
+sirmodel = SIRModel(graph, beta, gamma)
+
+# # Model Configuration
+# cfg = mc.Configuration()
+# cfg.add_model_parameter('beta', beta)
+# cfg.add_model_parameter('gamma', gamma)
+#
+# # cfg.add_model_parameter("percentage_infected", percentage_infected)
+# cfg.add_model_initial_configuration("Infected", [0,51, 100, 500, 4000, 131])
+# # set initial status for the model
+# model.set_initial_status(cfg)
 
 
 #new_range  = [i * i          for i in range(5)   if i % 2 == 0]
@@ -13,12 +38,8 @@ graph = Graph.Read_Ncol('moreno_highschool.txt', directed=False)
 # for v in graph.neighbors(graph.vs.find('19')):
 #     print graph.vs[v]['name']
 
-beta = 0.2857
-gamma = 0.1428
 
-sirmodel = SIRModel(graph, beta, gamma)
-
-#print graph
+# print graph
 
 # print sirmodel.beta
 # print sirmodel.gamma
@@ -26,7 +47,7 @@ sirmodel = SIRModel(graph, beta, gamma)
 #     print n
 #
 # print "\n########################\n"
-sirmodel.infect([0, 1])
+#sirmodel.infect([0, 51])
 #
 # for n in sirmodel.nodes_neighbors:
 #     print n
@@ -40,16 +61,41 @@ sirmodel.infect([0, 1])
 # for n in sirmodel.nodes_neighbors:
 #     print n
 
-#sirmodel.recover([1,0,68,69])
-#sirmodel.infect([0])
-print sirmodel.iterate()
+# sirmodel.recover([1,0,68,69])
+sirmodel.infect([0,51, 100, 500, 4000, 131])
 
+S = []
+I = []
+R = []
+
+for i in range(50):
+    #iteration = model.iteration()
+    iteration = sirmodel.iterate()
+
+    print iteration
+
+    # S.append(iteration['node_count'][0])
+    # I.append(iteration['node_count'][1])
+    # R.append(iteration['node_count'][2])
+    S.append(iteration[0])
+    I.append(iteration[1])
+    R.append(iteration[2])
+
+
+plt.plot(range(len(S)), S, 'g-')
+plt.plot(range(len(I)), I, 'r-')
+plt.plot(range(len(R)), R, 'b-')
+plt.axis([0, 50, 0, 36692])
+plt.xlabel('Geracoes')
+plt.ylabel('Individuos Infectados')
+plt.title('GASIR - Genetic Algorithm for SIR Model - Melhor Caso')
+plt.show()
 
 # print "\n########################\n"
 #
 # for n in sirmodel.nodes_neighbors:
 #     print n
 
-print "S: ", sirmodel.susceptibles
-print "I: ", sirmodel.infecteds
-print "R: ", sirmodel.recovereds
+# print "S: ", sirmodel.susceptibles
+# print "I: ", sirmodel.infecteds
+# print "R: ", sirmodel.recovereds
